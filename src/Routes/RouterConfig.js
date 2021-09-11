24ae43bfb776a611cc1c32241e270db89239bb00
amanpra123 <amanpra333@gmail.com>
2021-09-11 13:17:59 +0530

    successful merge

--- src/Routes/RouterConfig.js
+++ src/Routes/RouterConfig.js
@@ -2,11 +2,14 @@ import React from "react";
 import { Switch, Route } from "react-router-dom";
 import Register from "../Pages/Auth/Register";
 import Home from "../Pages/Public/Home";
+import Login from "../LoginPage/Login"
+
 const RouterConfig = () => {
   return (
     <div>
       <Switch>
         <Route path="/" component={Home} exact />
+        <Route path="/login" component={Login} />
         <Route path="/register" component={Register}/>
       </Switch>
     </div>
